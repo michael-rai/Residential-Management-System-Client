651a595fdaafbc5602d702d93a19fdba9bb0d1c5
michael-rai <michaelrai.prgmr@gmail.com>
2019-01-17 15:30:08 -0500

    set up table for index

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -72,10 +72,29 @@ const pwChgFailure = error => {
 
 // view maintenance index tix success and failures
 const viewMaintTixSuccess = data => {
+  let id = ''
+  let sub = ''
+  let des = ''
+  let res = ''
   data.tickets.map(ticket => {
-    $('#ticketIndexResponse').text(ticket)
+    `<th scope="row">${ticket.id}</th>
+    <td>${ticket.subject}</td>
+    <td>${ticket.description}</td>
+    <td>${ticket.resolved}</td>`
+
+    // id += `<div class=${ticket.id}>${ticket.id}</div>`
+    // // id += `<div>`
+    // sub += `<div class=${ticket.subject}>${ticket.subject}</div>`
+    // // sub += `<div>`
+    // des += `<div class=${ticket.description}>${ticket.description}</div>`
+    // // des += `<div>`
+    // res += `<div class=${ticket.resolved}>${ticket.resolved}</div>`
+    // // res += `<div>`
+    $('#ticketIndexIdResponse').html(id)
+    $('#ticketIndexSubResponse').html(sub)
+    $('#ticketIndexDesResponse').html(des)
+    $('#ticketIndexResolvedResponse').html(res)
   })
-  console.log(data.tickets)
 }
 
 // const viewMaintTixFailure = data => {
